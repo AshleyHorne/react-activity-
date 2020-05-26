ceb3b20814bd87a615f8c9767ff99dd2a87e4b93
Ashley Horne <ashleyhorne348@yahoo.com>
2020-05-26 14:17:01 -0400

    Set up
    -Added public and src folders
    -installed npm i
    -added todoi file

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Todo List </h1>
+      <h2>Here you can add and remove tasks</h2>
+      
+    </div>
+  );
+}

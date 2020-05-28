877db69a4551fcd8e8b2bee26fd952a97b1906ce
Ashley Horne <ashleyhorne348@yahoo.com>
2020-05-28 11:28:22 -0400

    Redo
    - Kept runing it errors, like: Error from chokidar (C:\): Error: EBUSY: resource busy or locked, lstat 'C:\swapfile.sys'
    
    -Was able to fix it by removing some old files and editing the package-json file again.  I redid some if the files but under different names
    
    - Now using Functions (name) () instead of class (name)

---
+++ src/Component/Content.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+function Content(){ 
+    return(
+      <p>fake content here</p>
+    )
+
+}
+export default Content;
--- src/TodoList.js
+++
@@ -1,6 +0,0 @@
-
-import React, { Component } from "react";
-import'./styles.css';
-
-
-export default TodoList;
